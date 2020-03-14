a2af0f655e82617dc6174cd87a91c93189186700
Giuseppe Maggiore <giuseppemag@gmail.com>
2020-03-14 13:55:29 +0100

    Most operators implemented. Join still missing. Minor aspects of type safety could be improved.

---
+++ app.js
@@ -0,0 +1,129 @@
+"use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const immutable_1 = require("immutable");
+const Without = (key, _a) => {
+    var _b = key, _ = _a[_b], values = __rest(_a, [typeof _b === "symbol" ? _b : _b + ""]);
+    return values;
+};
+const Rename = (keyOld, keyNew, _a) => {
+    var _b = keyOld, value = _a[_b], values = __rest(_a, [typeof _b === "symbol" ? _b : _b + ""]);
+    return (Object.assign(Object.assign({}, values), { [keyNew]: value }));
+};
+const MakePair = (t, u) => [t, u];
+const Relation = (values) => ({
+    Inverted: () => {
+        let invertedValues = immutable_1.Map();
+        const emptySet = immutable_1.Set();
+        values.forEach((k2s, k1) => k2s.forEach(k2 => invertedValues = invertedValues.update(k2, emptySet, x => x.add(k1))));
+        return Relation(invertedValues);
+    },
+    values: () => values
+});
+const Entity = () => (values, relations) => ({
+    idAs: (idKey, idKeyNew) => null,
+    // Entity<Db>()(values.mapKeys(id => Rename<Record<Id>, idKey, idKeyNew>(idKey, idKeyNew, id)), relations),
+    fieldAs: (key, keyNew) => Entity()(values.map(f => Rename(key, keyNew, f)), relations),
+    select: (...keys) => Entity()(values.map(f => keys.reduce((a, e) => (Object.assign(Object.assign({}, a), { [e]: f[e] })), {})), relations),
+    filter: (p) => Entity()(values.filter(p), relations),
+    expand: function (db, relation, q) {
+        return this.expandAs(db, relation, relation, q);
+    },
+    expandAs: (db, relation, as, q) => {
+        const allLinks = relations[relation].values();
+        const allTargets = db.from(relation).values();
+        const res = Entity()(values.map((fields, id) => {
+            const links = allLinks.get(id) || immutable_1.Set();
+            const y = links.reduce((y, targetId) => y.set(targetId, allTargets.get(targetId)), immutable_1.Map());
+            return Object.assign(Object.assign({}, fields), Rename(relation, as, { [relation]: y }));
+        }), Without(relation, relations));
+        return res;
+    },
+    join: (db, relation, q) => null,
+    values: () => values
+});
+const Database = (entities) => ({
+    from: (entity) => entities[entity]
+});
+const PersonId = immutable_1.Record({ PersonId: 0 });
+const AddressId = immutable_1.Record({ AddressId: 0 });
+const CityId = immutable_1.Record({ CityId: 0 });
+const personAddresses = Relation(immutable_1.Map()
+    .set(PersonId({ PersonId: 0 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 0 })))
+    .set(PersonId({ PersonId: 1 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 0 })))
+    .set(PersonId({ PersonId: 2 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 3 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 4 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 5 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 6 }), immutable_1.Set()
+    .add(AddressId({ AddressId: 2 }))));
+const addressCities = Relation(immutable_1.Map()
+    .set(AddressId({ AddressId: 0 }), immutable_1.Set()
+    .add(CityId({ CityId: 0 })))
+    .set(AddressId({ AddressId: 1 }), immutable_1.Set()
+    .add(CityId({ CityId: 1 })))
+    .set(AddressId({ AddressId: 2 }), immutable_1.Set()
+    .add(CityId({ CityId: 2 }))));
+const myEntities = {
+    People: Entity()(immutable_1.Map()
+        .set(PersonId({ PersonId: 0 }), { Name: "John", Surname: "Doe", Age: 27 })
+        .set(PersonId({ PersonId: 1 }), { Name: "Jane", Surname: "Doe", Age: 31 })
+        .set(PersonId({ PersonId: 2 }), { Name: "Giuseppe", Surname: "Maggiore", Age: 35 })
+        .set(PersonId({ PersonId: 3 }), { Name: "Giulia", Surname: "Costantini", Age: 34 })
+        .set(PersonId({ PersonId: 4 }), { Name: "Rebecca Sofia", Surname: "Maggiore", Age: 9 })
+        .set(PersonId({ PersonId: 5 }), { Name: "Rachel", Surname: "Maggiore", Age: 3 })
+        .set(PersonId({ PersonId: 6 }), { Name: "Francesco", Surname: "Di Giacomo", Age: 35 }), {
+        Addresses: personAddresses
+    }),
+    Addresses: Entity()(immutable_1.Map()
+        .set(AddressId({ AddressId: 0 }), { Street: "Afrikaanderplein", Number: 7, Postcode: "3072 EA" })
+        .set(AddressId({ AddressId: 1 }), { Street: "Kalverstraat", Number: 92, Postcode: "1012 PH" })
+        .set(AddressId({ AddressId: 2 }), { Street: "Heidelaan", Number: 139, Postcode: "3851 EX" }), {
+        People: personAddresses.Inverted(),
+        Cities: addressCities,
+    }),
+    Cities: Entity()(immutable_1.Map()
+        .set(CityId({ CityId: 0 }), { Name: "Rotterdam", Population: 1250000 })
+        .set(CityId({ CityId: 1 }), { Name: "Amsterdam", Population: 250000 })
+        .set(CityId({ CityId: 2 }), { Name: "Hengelo", Population: 150000 }), {
+        Addresses: addressCities.Inverted()
+    })
+};
+const db = Database(myEntities);
+const x0 = db.from("People").fieldAs("Name", "Nome").select("Nome").filter(p => p.Nome.startsWith("Giu"));
+const x1 = db.from("People").expand(db, "Addresses", a => a.select("Street", "Number"));
+//const x2 = db.from("People").join(db, "Addresses", a => a.select("Street", "Number")).join(db, "Cities", c => c.fieldAs("Name", "CityName").idAs("CityId", "UUIDOfCity"))
+const x3 = db.from("Cities").expand(db, "Addresses", a => a.expand(db, "People", p => p));
+const x4 = db.from("Cities").expand(db, "Addresses", a => a.expandAs(db, "People", "Inhabitants", p => p));
+const v0 = x0.values().toArray();
+const v1 = x1.values().toArray().map(x => (Object.assign(Object.assign({}, x[1]), { Addresses: x[1].Addresses.toArray().map(a => a[1]) })));
+//const v2 = x2.values().toArray()
+const v3 = x3.values().toArray().map(x => (Object.assign(Object.assign({}, x[1]), { Addresses: x[1].Addresses.toArray().map(a => a[1]) })));
+//const v4 = x4.values().toArray()
+console.log("Done");
+/* todo:
+ * actual implementation of methods
+    idAs
+    expandAs: <r extends keyof Relations, as extends string, Id2, Fields2, Relations2>(relation: r, as: as, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) :
+    join: <r extends keyof Relations, Id2, Fields2, Relations2>(relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) :
+ * define better constraints on Relations in Entity and expand, expandAs, and join?
+ * odataParser to create a database
+ * readme!!!
+ */
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,49 +1,244 @@
-type Fun<a,b> = (_:a) => b
-type Primitives = { string: string, number: number, boolean: boolean }
+import { Record, List, Map, Set, is } from "immutable"
 
-type Flattener<currentEntity extends string, currentFields, flatResult, relations> = {
-  withField: <a extends string, t extends keyof Primitives>(attribute: a, type: t) =>
-    Flattener<currentEntity, currentFields & { [x in a]: Primitives[t] }, flatResult, relations>
-  withMany: <r extends string, nestedResult, nestedFlatResult, relationsResult>(relation: r,
-    builder: Fun<Flattener<r, {}, flatResult, relations>, Flattener<r, nestedResult, nestedFlatResult, relationsResult>>) =>
-    Flattener<currentEntity, currentFields,
-      nestedFlatResult & { [x in r]: nestedResult[] },
-      relationsResult & { [x in currentEntity]: { [target in r]:"many" } }>
-  withOne: <r extends string, nestedResult, nestedFlatResult, relationsResult>(relation: r,
-    builder: Fun<Flattener<r, {}, flatResult, relations>, Flattener<r, nestedResult, nestedFlatResult, relationsResult>>) =>
-    Flattener<currentEntity, currentFields,
-      nestedFlatResult & { [x in r]: nestedResult[] },
-      relationsResult & { [x in currentEntity]: { [target in r]: "one" } }>
-  run: () => flatResult & { [x in currentEntity]: currentFields[] } & { relations: relations }
+type Without<T, K> = Pick<T, Exclude<keyof T, K>>
+type Fun<a, b> = (_: a) => b
+
+const Without = <T, K extends keyof T>(key: K, { [key]: _, ...values }: T): Without<T, K> => values
+const Rename = <T, KOld extends keyof T, KNew extends string>(keyOld: KOld, keyNew: KNew, { [keyOld]: value, ...values }: T):
+  Without<T, KOld> & { [x in KNew]: T[KOld] } => 
+  ({ ...values, ...{ [keyNew]: value } as { [x in KNew]: T[KOld] } })
+
+type EntityId<E> =
+  E extends Entity<infer Id, infer Fields, infer Relations, infer AllEntities> ?
+  Id : never
+
+type EntityRelations<E> =
+  E extends Entity<infer Id, infer Fields, infer Relations, infer AllEntities> ?
+  Relations : never
+
+type InvertedAriety<Ariety> =
+  Ariety extends "1-N" ? "N-1" : Ariety extends "N-1" ? "1-N" : Ariety
+
+type Inverted<R> =
+  R extends Relation<infer Db, infer Source, infer Target, infer Ariety> ?
+  Relation<Db, Target, Source, InvertedAriety<Ariety>>
+  : never
+
+type SourceName<R> =
+  R extends Relation<infer Db, infer SourceName, infer TargetName, infer Ariety> ?
+  SourceName : never
+
+type TargetName<R> =
+  R extends Relation<infer Db, infer SourceName, infer TargetName, infer Ariety> ?
+  TargetName : never
+
+type Source<R> =
+  R extends Relation<infer Db, infer SourceName, infer TargetName, infer Ariety> ?
+  Db[SourceName] : never
+
+type Target<R> =
+  R extends Relation<infer Db, infer SourceName, infer TargetName, infer Ariety> ?
+  Db[TargetName] : never
+
+interface Relation<Db, SourceName extends keyof Db, TargetName extends keyof Db, Ariety> {
+  Inverted: () => Inverted<Relation<Db, SourceName, TargetName, Ariety>>
+  values: () => Map<Record<EntityId<Db[SourceName]>>, Set<Record<EntityId<Db[TargetName]>>>>
 }
 
-/*
- * relations in type
- * multiple relations from same source in type
- * self-references
- * runtime (flattener)
- * re-hierarchify types
- * runtime (re-hierarchifier)
- */
+interface Entity<Id extends  Object, Fields, Relations, Db> {
+  idAs: <idKey extends keyof Id, idKeyNew extends string>(idKey: idKey, idKeyNew: idKeyNew) => Entity<Without<Id, idKey> & { [x in idKeyNew]: Id[idKey] }, Fields, Relations, Db>
+  fieldAs: <k extends keyof Fields, kNew extends string>(key: k, keyNew:kNew) => Entity<Id, Without<Fields, k> & { [x in kNew]:Fields[k] }, Relations, Db>
+  select: <k extends keyof Fields>(...keys: k[]) => Entity<Id, Pick<Fields, k>, Relations, Db>
+  filter: (p: (fields:Fields, id:Record<Id>) => boolean) => Entity<Id, Fields, Relations, Db>
+  expand: <r extends keyof Relations & string, Id2, Fields2, Relations2>(db: Database<Db>, relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) =>
+      Entity<Id, Fields & { [x in TargetName<Relations[r]>]: Map<Id2, Fields2> }, Without<Relations, r>, Db>
+  expandAs: <r extends keyof Relations & string, as extends string, Id2, Fields2, Relations2>(db: Database<Db>, relation: r, as:as, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) =>
+      Entity<Id, Fields & { [x in as]: Map<Id2, Fields2> }, Without<Relations, r>, Db>
+  join: <r extends keyof Relations, Id2, Fields2, Relations2>(db: Database<Db>, relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) =>
+      Entity<Id & Id2, Fields & Fields2, Relations & Relations2, Db>
+  values: () => Map<Record<Id>, Fields>
+}
 
-let f: Flattener<"person", {}, {}, {}> = null!
+interface Database<Entities> {
+  from: <E extends keyof Entities>(entity: E) => Entities[E]
+}
 
-const result = f
-  .withField("name", "string")
-  .withField("surname", "string")
-  .withField("age", "number")
-  .withOne("city", b => b
-    .withField("name", "string")
-    .withField("population", "number")
-    .withOne("country", b =>
-      b.withField("name", "string")
-        .withField("continent", "string")
+const MakePair = <t, u>(t: t, u: u): [t, u] => [t, u]
+
+const Relation = <Db, SourceName extends keyof Db, TargetName extends keyof Db, Ariety> (values: Map < Record < EntityId < Db[SourceName] >>, Set <Record<EntityId<Db[TargetName]>>>>): Relation<Db, SourceName, TargetName, Ariety> => ({
+  Inverted: (): Inverted<Relation<Db, SourceName, TargetName, Ariety>> => {
+    let invertedValues = Map<Record<EntityId<Db[TargetName]>>, Set<Record<EntityId<Db[SourceName]>>>>()
+    const emptySet = Set()
+    values.forEach((k2s, k1) => 
+      k2s.forEach(k2 =>
+        invertedValues = invertedValues.update(k2, emptySet, x => x.add(k1))
+      )
     )
-  )
-  .withMany("employer", b => b
-    .withField("name", "string")
-    .withField("employees", "number")
-  )
-  .run()
+    return Relation<Db, TargetName, SourceName, InvertedAriety<Ariety>>(invertedValues)
+  },
+  values: () => values
+})
 
-const x: typeof result = null!
+const Entity = <Db>() => <Id, Fields, Relations>(values:Map<Record<Id>, Fields>, relations: Relations): Entity<Id, Fields, Relations, Db> => ({
+  idAs: <idKey extends keyof Id, idKeyNew extends string>(idKey: idKey, idKeyNew: idKeyNew):
+    Entity<Without<Id, idKey> & { [x in idKeyNew]: Id[idKey] }, Fields, Relations, Db> =>
+    null!,
+    // Entity<Db>()(values.mapKeys(id => Rename<Record<Id>, idKey, idKeyNew>(idKey, idKeyNew, id)), relations),
+  fieldAs: <k extends keyof Fields, kNew extends string>(key: k, keyNew: kNew):
+    Entity<Id, Without<Fields, k> & { [x in kNew]: Fields[k] }, Relations, Db> =>
+    Entity<Db>()(values.map(f => Rename<Fields, k, kNew>(key, keyNew, f)), relations),
+  select: <k extends keyof Fields>(...keys: k[]): Entity<Id, Pick<Fields, k>, Relations, Db> =>
+    Entity<Db>()(values.map(f => keys.reduce((a, e) => ({ ...a, [e]: f[e] }), {}) as Pick<Fields, k>), relations),
+  filter: (p: (fields: Fields, id: Record<Id>) => boolean): Entity<Id, Fields, Relations, Db> =>
+    Entity<Db>()(values.filter(p), relations),
+  expand: function <r extends keyof Relations & string, Id2, Fields2, Relations2>(this, db: Database<Db>, relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>):
+    Entity<Id, Fields & { [x in TargetName<Relations[r]>]: Map<Id2, Fields2> }, Without<Relations, r>, Db> {
+    return this.expandAs(db, relation, relation, q)
+  },
+  expandAs: <r extends keyof Relations & string, as extends string, Id2, Fields2, Relations2>(db: Database<Db>, relation: r, as: as, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) :
+    Entity<Id, Fields & { [x in as]: Map<Id2, Fields2> }, Without<Relations, r>, Db> => {
+    const allLinks = (relations[relation] as any).values()
+    const allTargets = (db.from(relation as any) as any).values()
+    const res = Entity<Db>()(values.map((fields, id) => {
+      const links = allLinks.get(id) || Set()
+      const y = links.reduce((y: any, targetId: any) => y.set(targetId, allTargets.get(targetId)), Map())
+      return {...fields, ...Rename(relation, as, { [relation]: y })}
+    }), Without(relation, relations))
+    return res
+  },
+  join: <r extends keyof Relations, Id2, Fields2, Relations2>(db: Database<Db>, relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) :
+    Entity<Id & Id2, Fields & Fields2, Relations & Relations2, Db> => {
+    //const allLinks = (relations[relation] as any).values()
+    //const allTargets = (db.from(relation as any) as any).values()
+    //const res = Entity<Db>()(values.map((fields, id) => {
+    //  const links = allLinks.get(id) || Set()
+    //  const y = links.reduce((y: any, targetId: any) => y.set(targetId, allTargets.get(targetId)), Map())
+    //  return { ...fields, ...Rename(relation, as, { [relation]: y }) }
+    //}), Without(relation, relations))
+    return null!
+  },
+  values: () : Map<Record<Id>, Fields> => values
+})
+
+const Database = <Entities>(entities: Entities): Database<Entities> => ({
+  from: <E extends keyof Entities>(entity: E): Entities[E] => entities[entity]
+})
+
+
+interface Person {
+  Name: string
+  Surname: string
+  Age: number
+}
+
+interface Address {
+  Street: string
+  Number: number
+  Postcode: string
+}
+
+interface City {
+  Name: string
+  Population: number
+}
+
+interface MyEntities {
+  People: Entity<{ PersonId: number }, Person, {
+    Addresses: Relation<MyEntities, "People", "Addresses", "1-1">
+  }, MyEntities>
+  Addresses: Entity<{ AddressId: number }, Address, {
+    Cities: Relation<MyEntities, "Addresses", "Cities", "N-1">,
+    People: Inverted<EntityRelations<MyEntities["People"]>["Addresses"]>
+  }, MyEntities>
+  Cities: Entity<{ CityId: number }, City, {
+    Addresses: Inverted<EntityRelations<MyEntities["Addresses"]>["Cities"]>
+  }, MyEntities>
+}
+
+const PersonId = Record({ PersonId: 0 })
+const AddressId = Record({ AddressId: 0 })
+const CityId = Record({ CityId: 0 })
+
+const personAddresses = Relation<MyEntities, "People", "Addresses", "N-1">(
+  Map<Record<{ PersonId: number }>, Set<Record<{ AddressId: number }>>>()
+    .set(PersonId({ PersonId: 0 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 0 })))
+    .set(PersonId({ PersonId: 1 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 0 })))
+    .set(PersonId({ PersonId: 2 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 3 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 4 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 5 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 1 })))
+    .set(PersonId({ PersonId: 6 }), Set<Record<{ AddressId: number }>>()
+      .add(AddressId({ AddressId: 2 })))
+)
+
+const addressCities = Relation<MyEntities, "Addresses", "Cities", "N-1">(
+  Map<Record<{ AddressId: number }>, Set<Record<{ CityId: number }>>>()
+    .set(AddressId({ AddressId: 0 }), Set<Record<{ CityId: number }>>()
+      .add(CityId({ CityId: 0 })))
+    .set(AddressId({ AddressId: 1 }), Set<Record<{ CityId: number }>>()
+      .add(CityId({ CityId: 1 })))
+    .set(AddressId({ AddressId: 2 }), Set<Record<{ CityId: number }>>()
+      .add(CityId({ CityId: 2 })))
+)
+
+const myEntities: MyEntities = {
+  People: Entity<MyEntities>()(Map<Record<{ PersonId: number }>, Person>()
+    .set(PersonId({ PersonId: 0 }), { Name: "John", Surname: "Doe", Age: 27 })
+    .set(PersonId({ PersonId: 1 }), { Name: "Jane", Surname: "Doe", Age: 31 })
+    .set(PersonId({ PersonId: 2 }), { Name: "Giuseppe", Surname: "Maggiore", Age: 35})
+    .set(PersonId({ PersonId: 3 }), { Name: "Giulia", Surname: "Costantini", Age: 34 })
+    .set(PersonId({ PersonId: 4 }), { Name: "Rebecca Sofia", Surname: "Maggiore", Age: 9 })
+    .set(PersonId({ PersonId: 5 }), { Name: "Rachel", Surname: "Maggiore", Age: 3 })
+    .set(PersonId({ PersonId: 6 }), { Name: "Francesco", Surname: "Di Giacomo", Age: 35 })
+  ,
+  {
+    Addresses: personAddresses
+  }),
+  Addresses: Entity<MyEntities>()(Map<Record<{ AddressId: number }>, Address>()
+    .set(AddressId({ AddressId: 0 }), { Street: "Afrikaanderplein", Number: 7, Postcode: "3072 EA" })
+    .set(AddressId({ AddressId: 1 }), { Street: "Kalverstraat", Number: 92, Postcode: "1012 PH" })
+    .set(AddressId({ AddressId: 2 }), { Street: "Heidelaan", Number: 139, Postcode: "3851 EX" })
+  ,
+  {
+    People: personAddresses.Inverted(),
+    Cities: addressCities,
+  }),
+  Cities: Entity<MyEntities>()(Map<Record<{ CityId: number }>, City>()
+    .set(CityId({ CityId: 0 }), { Name: "Rotterdam", Population: 1250000 })
+    .set(CityId({ CityId: 1 }), { Name: "Amsterdam", Population: 250000 })
+    .set(CityId({ CityId: 2 }), { Name: "Hengelo", Population: 150000 })
+  ,
+  {
+    Addresses: addressCities.Inverted()
+  })
+}
+
+const db: Database<MyEntities> = Database(myEntities)
+const x0 = db.from("People").fieldAs("Name", "Nome").select("Nome").filter(p => p.Nome.startsWith("Giu"))
+const x1 = db.from("People").expand(db, "Addresses", a => a.select("Street", "Number"))
+//const x2 = db.from("People").join(db, "Addresses", a => a.select("Street", "Number")).join(db, "Cities", c => c.fieldAs("Name", "CityName").idAs("CityId", "UUIDOfCity"))
+const x3 = db.from("Cities").expand(db, "Addresses", a => a.expand(db, "People", p => p))
+const x4 = db.from("Cities").expand(db, "Addresses", a => a.expandAs(db, "People", "Inhabitants", p => p))
+
+const v0 = x0.values().toArray()
+const v1 = x1.values().toArray().map(x => ({ ...x[1], Addresses: x[1].Addresses.toArray().map(a => a[1]) }))
+//const v2 = x2.values().toArray()
+const v3 = x3.values().toArray().map(x => ({ ...x[1], Addresses: x[1].Addresses.toArray().map(a => a[1]) }))
+//const v4 = x4.values().toArray()
+
+console.log("Done")
+
+/* todo:
+ * actual implementation of methods
+    idAs
+    join: <r extends keyof Relations, Id2, Fields2, Relations2>(relation: r, q: Fun<Target<Relations[r]>, Entity<Id2, Fields2, Relations2, Db>>) :
+ * define better constraints on Relations in Entity and expand, expandAs, and join?
+ * odataParser to create a database
+ * readme!!!
+ */
